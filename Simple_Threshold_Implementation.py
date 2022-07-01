e82723c6c281fae0174e5347199bf15427586d26
aswin <aswinpdas83@gmail.com>
2022-07-01 11:48:35 +0530

    threshold implimentation of a picture

--- Simple_Threshold_Implementation.py
+++ Simple_Threshold_Implementation.py
@@ -1,2 +1,7 @@
 import cv2
-cv2.
+image = cv2.imread('C:/Users/aswin/PycharmProjects/opencv_project/gradient.jpg',1)
+ret,thresh = cv2.threshold(image,125,255,cv2.THRESH_BINARY)
+cv2.imshow('image',image)
+cv2.imshow('thresh',thresh)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
